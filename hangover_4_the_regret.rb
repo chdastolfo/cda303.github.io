f7d433baed28038274154d151b59ef56cd61de77
chdastolfo <cda303@nyu.edu>
2016-02-24 20:11:17 -0500

    Added files via upload

--- hangover_4_the_regret.rb
+++ hangover_4_the_regret.rb
@@ -1,147 +1,129 @@
-
+#require map.rb
 
 class Player
 	attr_accessor :player_health, :player_attack, :player_defense, :player_inventory
+	
 	def initialize
 		@player_health = 20
 		@player_attack = 10
 		@player_defense = 10
+
+		@player_inventory = []
+
+		puts "Player's health is #{player_health}. Player's attack is #{player_attack} and player's defense is #{player_defense}."
 	end
 
-	@player_inventory = []
+	def check_inventory
+		puts @player_inventory
+	end
 
 	def take(item)
 		@player_inventory.push(item)
 	end
 
-	def look
-		puts room.description
-	end
-
-	def check_inventory
-		puts player_inventory
-	end
-
 end
 
-	#class Baron
-	#include Player
-	#attr_accessor :baron_health
+class Game
+	def initialize
+		start()
+	end
 
-	#def initialize
-		#@player_health = player_health
-		#@player_attack = player_attack
-	#	@player_defense = player_defense
-	#end
+	def prompt
+  	print "Enter command > "
+	end
 
-		#if @player_attack >= 20 || @player_defense >= 15
-			#@baron_health = 25
-		#elsif @player_attack >= 20 && @player_defense >=15
-			#@baron_health = 20
-		#else
-			#@baron_health = 30
-		#end
+	def start
+		Player.new
+		room_1
+	end
 
-	#end
+	def take(item)
+		@player_inventory.push(item)
+	end
 
+	def set_baron_health
+		if @player_attack > 19 || @player_defense > 14
+			@baron_health = 25
+		elsif @player_attack > 19 && @player_defense >14
+			@baron_health = 20
+		else
+			@baron_health = 30
+		end
+	end
 
-
-class Room
-
-def Room_1
-		prompt()
-		object = gets.chomp.upcase
-		direction = gets.chomp.upcase
-	
-	case direction
+	def room_1
+		puts "You awake to find yourself in a dark, musty room. In this room are an empty FLAGON, a suspicious looking HAYSTACK, and a sharp ROCK. 
+		Visible exits are NORTH, WEST, and TROGDOR."
+		prompt
+		answer = $stdin.gets.chomp.upcase
+	case answer
 	when 'NORTH'
-		Room_2
+		room_2
 	when 'WEST'
-		Room_3
+		room_3
 	when 'TROGDOR'
 		puts "Really? You had to know you'd lose a fight with The Burninator. You're dead."
 		you_lose
-	else
-		error_message
-	end
-
-	case object
 	when 'FLAGON'
-		puts "It seems like there yet be some moonshine left in this here flagon! Will ye drink it? Type 'y/n'. "
-		choice = gets.chomp.downcase
-			if choice == 'y'
-				@player_attack += 5
-				puts "Your attack power has increased by 5, because you're an aggressive drunk. Your attack is now #{attack}."
-				take(flagon)
-			else
-				Room_1
-			end
+		puts "It seems like there yet be some moonshine left in this here flagon!"
+			@player.player_attack += 5
+			puts "Your attack power has increased by 5, because you're an aggressive drunk. Your attack is now #{@player_attack}."
+			take(flagon)
 	when 'HAYSTACK'
 		puts "'Suspicious: def. An object which, in the context of this game, will almost certainly kill you.' You're dead."
 		you_lose
 	when 'ROCK'
-		take(rock)
-		"You have put the rock into your inventory."
+		@player.take(rock)
+		puts "You have put the rock into your inventory."
+		check_inventory
 	else
 		error_message
+		room_1
 	end
-
 end
 
-def Room_2
+def room_2
 	puts "Looks like the kitchen. Inside, you see a truly enormous carving KNIFE, some CHICKEN, and a bottle of BOURBON. Exits are NORTH, SOUTH, and SOUTHWEST."
-
-direction = gets.chomp.upcase
-object = gets.chomp.upcase
-
-case direction
+	prompt
+	answer = $stdin.gets.chomp.upcase
+case answer
 when 'NORTH'
-	Room_4
+	room_4
 when 'SOUTH'
-	Room_1
+	room_1
 when 'SOUTHWEST'
-	Room_3
-else
-	error_message
-end
-
-case object
+	room_3
 	when 'KNIFE'
 		puts "Good thinking grabbing the stabby looking thing. Your attack power has increased by 5."
-		take(knife)
+		@player.take(knife)
 		@player_attack += 5
+		check_inventory
 	when 'CHICKEN'
 		puts "Your defense power has increased because... chicken."
 		take(chicken)
 		@player_defense += 5
+		check_inventory
 	when 'BOURBON'
 		puts "YE ARE EVEN MORE AGGRO."
-		take(bourbon)
+		@player.take(bourbon)
 		@player_attack += 5
+		check_inventory
 	else
 		error_message
+		room_2
 end
 
 end
 
-def Room_3
+def room_3
+	prompt
 	puts "You've reached the bathroom, and are confronted by an odour most foule. You see some BANDAGES, ASPIRIN, and a TOASTER floating inside of a bathtub. Exits are KANYE'S DAUGHTER and EAST."
-
-direction = gets.chomp.upcase
-object = gets.chomp.upcase
-
-case direction
-
+	answer = $stdin.gets.chomp.upcase
+case answer
 when 'NORTH WEST'
-	Room_2
+	room_2
 when 'EAST'
-	Room_1
-else
-	error_message
-end
-
-case object
-
+	room_1
 when 'BANDAGES'
 	puts "Your ability to not bleed to death has increased. Defense +5."
 	take(bandages)
@@ -154,19 +136,18 @@ when 'TOASTER'
 	you_lose
 else
 	error_message
+	room_3
 end
 
 end
 
-def Room_4
+def room_4
 	puts "You're in some sort of weird antechamber. There doesn't appear to be anything in here. Exits are NORTH and DENNIS."
-
-direction = gets.chomp.upcase
-
-case direction
-
+	prompt
+		answer = $stdin.gets.chomp.upcase
+case answer
 when 'NORTH'
-	Room_5
+	room_5
 when 'DENNIS'
 	puts "You're confronted by a surly man named DENNIS. He asks you for the password. You've... got the password... right?"
 	password = gets.chomp
@@ -176,71 +157,30 @@ when 'DENNIS'
 			puts "That's not the password! Uh-oh... Dennis looks angry. He killed you. Better luck next time."
 			you_lose
 		end
-else
+	else
 	error_message
+	room_4
+end
 end
 
-end
-
-def Room_5
+def room_5
 	trigger_boss_battle
 end
 
-end
-
-class Map
-
-end
-
-
-class Game
-	def initialize
-		start()
-end
-
-	def prompt
-  	print "Enter command > "
-  	answer = gets.chomp
-	end
-	
-
-	def start
-		Room.new
-		Player.new
-		puts "You awake to find yourself in a dark, musty room. In this room are an empty FLAGON, a suspicious looking HAYSTACK, and a sharp ROCK. 
-		Visible exits are NORTH, WEST, and TROGDOR."
-		Room_1
-	end
-
 	def trigger_boss_battle
+		Baron.new
 		puts "You've encountered the fearson Baron Von Schneckeldorfing. You must roll the direction
 		to determine your fate. Roll higher than the Baron to chip away at his health. If your roll is lower, you're hit instead.
-		You lose if your health reaches 0. Your health is #{player_health}."
-		Baron.new
-		dice_roll
+		You lose if your health reaches 0. Your health is #{@player.player_health}."
+		@baron.attack_baron
 	end
 
 	def you_win
-		puts "You've escaped the dungeon in (mostly) one piece! Congrats!"
+		abort("You've escaped the dungeon in (mostly) one piece! Congrats! Run the file to play again.")
 	end
 
 	def you_lose
-		puts "Game Over."
-		#alive?! = false
-	end
-
-	def dice_roll
-		player_roll = 1 + rand(6)
-		baron_roll = 1 + rand(6)
-
-	until @player_health <= 0 || @baron_health <= 0
-
-		if player_roll > baron_roll
-			baron_health -= player_roll
-		else
-			player_health -= baron_roll
-		end
-		puts "You rolled a #{player_roll}. The Baron rolled a #{baron_roll}. Your health is now #{player_health}. The baron's health is #{baron_health}."
+		abort("Game Over.")
 	end
 
 def error_message
@@ -248,8 +188,66 @@ def error_message
 	end
 end
 
+	#def dice_roll
+	#	player_roll = 1 + rand(6)
+	#	baron_roll = 1 + rand(6)
+	#	puts "You rolled a #{player_roll}. The Baron rolled a #{baron_roll}. Your health is now #{player_health}. The baron's health is #{baron_health}."
+	#end
 
+class Baron
 
+attr_accessor :baron_health
+
+@baron_health = 30
+
+def initialize
+	#set_baron_health
+end
+
+def attack_baron
+	# Generate random number between 0 and 6
+	# and round down to either 0 or 5
+	# store resulting integer in variable roll
+	player_roll = rand(6).floor
+ 
+	player_damage = player_roll * 5
+	puts "\nYou attack the Baron for #{player_damage} damage."
+ 
+	@baron_health -= player_damage
+ 
+	if @baron_health <= 0
+		puts "\nYou defeated the Baron!"
+		you_win
+	else
+		puts "Baron's health: #{@baron_health}hp"
+		defend_baron
+	end
+end
+ 
+def defend_baron
+	# Generate random number between 0 and 6
+	# and round down to either 0 or 5
+	# store resulting integer in variable roll
+	baron_roll = rand(6).floor
+ 
+	baron_damage = baron_roll * 5
+	puts "\nBaron attacks you for #{baron_damage} damage."
+ 
+	@player_health -= baron_damage
+ 
+	if @player_health <= 0
+		you_lose
+	else
+		puts "Your health: #{@player_health}hp"
+		puts "\nContinue attacking? Y/N"
+		choice = $stdin.gets.chomp.upcase
+		if choice == 'Y'
+			attack_baron
+		else
+			you_lose
+		end
+	end
+end
 
 end
 
